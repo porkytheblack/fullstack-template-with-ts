65e509c68fffe188d7e5453cd84a6b67feba8312
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2022-06-15 11:03:49 +0300

    initialized styled-components

--- fullstack-with-antd-styledcomponents-tailwind/next.config.js
+++ fullstack-with-antd-styledcomponents-tailwind/next.config.js
@@ -8,6 +8,9 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  }
 })
 
 module.exports = withBundleAnalyzer()
--- fullstack-with-antd-styledcomponents-tailwind/pages/_app.tsx
+++ fullstack-with-antd-styledcomponents-tailwind/pages/_app.tsx
@@ -2,9 +2,30 @@ import 'antd/dist/antd.css'
 import '../styles/vars.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  *{
+    box-sizing: border-box;
+
+  }
+`
+
+const theme = {
+  colors: {
+    primary: "black"
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return    <>
+    <GlobalStyles/>
+  <ThemeProvider theme={theme} >
+            
+            <Component {...pageProps} />
+          </ThemeProvider> 
+          </>
 }
 
 export default MyApp
