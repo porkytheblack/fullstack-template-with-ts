6cf8d64fbc3f84b91b7f3fa2711c5a737540e21b
porkytheblack <porkytheblack@gmail.com>
2022-07-05 05:21:32 +0000

    changes

--- fullstack-with-antd-styledcomponents-tailwind/pages/index.tsx
+++ fullstack-with-antd-styledcomponents-tailwind/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Typography } from 'antd'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -5,8 +6,10 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      
+    <div className="bg-black flex flex-col items-center justify-center h-screen w-screen" >
+        <Typography.Text className="font-bold !text-white text-4xl " >
+          Wassup👋👋
+        </Typography.Text>
     </div>
   )
 }
